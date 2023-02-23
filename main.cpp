3c6c19aa679ccd44a2f090c00760f05653ec5579
ajb601 <90455297+ajb601@users.noreply.github.com>
2023-02-23 16:48:39 -0500

    started main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include "gradebook.h"
+
+int main(int argc, char* argv[]) {
+
+    const std::string fname = argv[1];
+
+    Course CSC212(fname);
+
+    std::string ss;
+    while(std::cin >> ss){
+        if()
+    }
+
+}
