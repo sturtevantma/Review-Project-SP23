f63a21e75b2fb25ce3ceba3e123b795de987a17e
sturtevantma <sturtevantma21@gmail.com>
2023-02-24 13:06:18 -0500

    Updated header file

--- gradebook.h
+++ gradebook.h
@@ -4,17 +4,19 @@
 #include <vector>
 
 struct Category {
-    std::string name;
-    float weight;
-    std::vector<std::pair<std::string, float> > grades;
+    std::string name;                       // The name of the category
+    float weight;                           // The category's weight for your course grade
+    std::vector<std::pair<std::string, float> > grades; // A vector of pairs containing all assignments and the achieved grades
 };
 
 class Course {
     private:
-        std::string name;
-        std::vector<Category> categories;
+        std::string name;                   // The name of the course
+        std::vector<Category> categories;   // Vector of all course categories. (See "Category")
 
     public:
+        Course();                           // Constructor to create empty gradebook
+        Course(std::fname);                 // Constructor to load grades from a file
         float current_grade();              // The users grade excluding incomplete work (represented with a -1 in the file)
         float cum_grade();                  // The users total grae including all ork (incomplete counted as 0)
         void read_file(std::string fname);  // Takes a file name and reads the data, invalid data will throw an exception
