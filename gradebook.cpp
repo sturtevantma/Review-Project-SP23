badef20a7da7e131e6790f8700ac981ba742edf2
hcz4 <hcz4th@gmail.com>
2023-02-24 14:47:59 -0500

    comment cpp

--- gradebook.cpp
+++ gradebook.cpp
@@ -84,44 +84,78 @@ float Course::search_grade(std::string name) {
 }
 
 float Course::current_grade() {
-    //
+    // Summation of only currently submitted grades
     float total_grade;
 
     for (int i = 0; i < categories.size(); i++) {
+        // Set the category specific grade to 0
         float category_grade = 0;
-        int size =  categories[i].grades.size();
+        // Set the size of the grades to a manipulatable ingteger so we can decrease as needed
+        int size = categories[i].grades.size();
+
         for (int j = 0; j < categories[i].grades.size(); j++){
             std::pair<std::string, float> check = categories[i].grades[j];
+            // If a -1 is found then we decrease the size because its not cummulative
             if (check.second == -1) {
                 size--;
                 continue;
             }
             category_grade += check.second;
         }
+    // Arithmetic to get category_grade to the correct avg and weight before adding to total_grade
     category_grade /= size;
+    category_grade *= (categories[i].weight/100);
+    total_grade += category_grade;
+    
+    }
+    return total_grade;
+}
+
+float Course::cum_grade() {
+    // Cummulative grade of all work submitted and non-submitted yet
+    float total_grade;
+    // Loop over all categories
+    for (int i = 0; i < categories.size(); i++) {
+        // Set the category specific grade to 0
+        float category_grade = 0;
+        // Loop over each category and sum the grades together
+        for (int j = 0; j < categories[i].grades.size(); j++){
+            std::pair<std::string, float> check = categories[i].grades[j];
+            // If -1 found, skip over b/c were dividing by the total number of grades anyways
+            if (check.second == -1) {
+                continue;
+            }
+            category_grade += check.second;
+        }
+    // Arithmetic before adding to total to make sure its the avg, then accounts for weight category is worth
+    category_grade /= categories[i].grades.size();
     category_grade *= (categories[i].weight/100);
     total_grade += category_grade;
     }
     return total_grade;
 }
 
-float Course::cum_grade() {
-    //
-    float total_grade;
-
-    for (int i = 0; i < categories.size(); i++) {
-        float category_grade = 0;
-        int size =  categories[i].grades.size();
-        for (int j = 0; j < categories[i].grades.size(); j++){
-            std::pair<std::string, float> check = categories[i].grades[j];
-            if (check.second == -1) {
-                continue;
-            }
-            category_grade += check.second;
+float Course::cat_grade(std::string name) {
+    // Instantiate an index to find the category from input name
+    int idx = 0;
+    for (int b = 0; b < categories.size(); b++) {
+        if (categories[b].name == name) {
+            idx = b;
+            break;
         }
-    category_grade /= size;
-    category_grade *= (categories[i].weight/100);
-    total_grade += category_grade;
     }
+
+    float total_grade;
+    // Loop over the categories grades and add them to total_grade if it's not a -1
+    for (int i = 0; i < categories[idx].grades.size(); i++) {
+        std::pair<std::string, float> check = categories[idx].grades[i];
+        if (check.second == -1) {
+            continue;
+        }
+        total_grade += check.second;
+    }
+    // Calculations for cummulative category grade
+    total_grade /= categories[idx].grades.size();
+    total_grade *= (categories[idx].weight/100);
     return total_grade;
 }
